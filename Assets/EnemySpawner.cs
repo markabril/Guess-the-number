748f096cd3f0195cc73cac10a0150ed0f85b5482
markabril <anthonyabril0827@gmail.com>
2023-01-10 13:12:27 +0800

    Add Spawner

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -23,7 +23,7 @@ public class EnemySpawner : MonoBehaviour
     private IEnumerator SpawnEnemy(float interval, GameObject obj)
     {
         yield return new WaitForSeconds(interval);
-        GameObject enemy = Instantiate(obj, new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-6f, 6f)), Quaternion.identity);
+        GameObject enemy = Instantiate(obj, new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-6f, 6f) ), Quaternion.identity);
         StartCoroutine(SpawnEnemy(interval, obj));
     }
 }
