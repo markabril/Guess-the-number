4093ef1bad77a87a37bea0a98c906ee3e75e8e12
markabril <anthonyabril0827@gmail.com>
2023-01-17 17:43:23 +0800

    adding animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,7 +17,12 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    private Animator animator;
     // Update is called once per frame
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();    
+    }
     void Update()
     {
         //float x = joystick.Horizontal;
@@ -26,7 +31,25 @@ public class PlayerMovement : MonoBehaviour
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        if (z < 0)
+        {
+            animator.SetBool("isBackward", true);
+            animator.SetBool("isForward", false);
+        }
+        else if(z > 0)
+        {
+            animator.SetBool("isBackward", false);
+            animator.SetBool("isForward", true);
+
+        }
+        else
+        {
+            animator.SetBool("isBackward", false);
+            animator.SetBool("isForward", false);
+        }
         Vector3 move = transform.right * x + transform.forward * z;
+        float magnitude = Mathf.Clamp01(move.magnitude) * movementSpeed;
+        move.Normalize();
 
 
         if(isGrounded && velocity.y < 0)
@@ -38,14 +61,30 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravity);
         }
+
+
+        if(move != Vector3.zero)
+        {
+            animator.SetBool("isMoving", true);
+            Player.Move(movementSpeed * Time.deltaTime * move);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
         Player.Move(velocity * Time.deltaTime);
 
         velocity.y -= gravity * Time.deltaTime;
-        Player.Move(movementSpeed * Time.deltaTime * move);
     }
         
 
     public void JumpPlayer()
+    {
+
+    }
+
+
+    public void getSpeed()
     {
 
     }
